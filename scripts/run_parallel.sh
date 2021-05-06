b902275756c242c10712a9a1701d9d18901d3e5d
Ra1issa <ra1issa@bu.edu>
2021-05-06 16:46:39 -0400

    Fix testing error

---
+++ scripts/run_parallel.sh
@@ -0,0 +1,6 @@
+echo "Starting Program"
+cargo build --release
+# cd ../src
+# cargo run --release --bin parallel-server &
+# sleep 1
+# cargo run --release --bin parallel-client &
---
+++ scripts/run_simple.sh
@@ -0,0 +1,6 @@
+echo "Starting Program"
+cargo build --release
+# cd ../src
+# cargo run --release --bin simple-server &
+# sleep 1
+# cargo run --release --bin simple-client &
