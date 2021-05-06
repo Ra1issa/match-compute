6ecfd9f1368b0928a1992e821e4f2c38c3ddd20d
Rawane Issa <rawaneissa@gmail.com>
2021-05-06 11:59:29 -0400

    Delete match-compute-parallel directory

--- match-compute-parallel/run_experiment.bat
+++
@@ -1,5 +0,0 @@
-echo "Starting Program"
-
-start cmd /c cargo run --release --bin parallel-server ^& pause
-timeout /t 1
-start cmd /c cargo run --release --bin parallel-client ^& pause
--- match-compute-parallel/run_experiment.sh
+++
@@ -1,9 +0,0 @@
-ps aux | grep "target/release/examples/"
-pkill -f "target/release/examples/"
-pkill -f "match-compute"
-
-echo "Starting Program"
-
-cargo run --release --bin parallel-server &
-sleep 1
-cargo run --release --bin parallel-client &
